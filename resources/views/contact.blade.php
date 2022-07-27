214dfab881cdfdb73265289105a80c30d7e54928
Adham <Adham-461@hotmail.com>
2022-07-27 17:00:31 +0300

    Done

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -38,9 +38,7 @@
         <div class="container">
             <div class="row">
                 <div class="col-6 col-md-7 map">
-                    <iframe
-                        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3623.525097389987!2d46.655882899999995!3d24.7431792!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3e2ee2a3f0446607%3A0xf26276486ff2996b!2sAwj%20Group%20LLC!5e0!3m2!1sen!2ssa!4v1653805099480!5m2!1sen!2ssa"
-                        frameborder="0" style="border:0; width:100%" allowfullscreen=""></iframe>
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d226.39194560305282!2d46.67227781960658!3d24.786167820519566!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3e2ee3a70733ccaf%3A0xac9fc41130481e41!2z2LTYsdmD2Kkg2KzZhtmJINin2YTYt9io2YrYqQ!5e0!3m2!1sen!2s!4v1658918901016!5m2!1sen!2s" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
                 </div>
                 <div class="col-6 col-md-5 form">
                     <h2>{{ __('index.73') }}</h2>
